d934dd190b6de9c328b3251ec3c3b1a0927258ec
varunyk1805 <varunyk1805@gmail.com>
2022-08-15 23:15:23 +0300

    add success hw_07

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+            <a class="gallery__link" href=${original}>
+                <img
+                    class="gallery__image"
+                    src=${preview}
+                    data-source=${original}
+                    alt=${description}
+                 />
+             </a>
+        </div>`
+}).join('');
+
+const showImageHandller = event => {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') return;
+    const url = event.target.getAttribute('data-source');
+    const instance = basicLightbox.create(
+        `<img src=${url} width="200" height="150">`
+    );
+
+    const pressEscapeHandller = event => {
+        if (event.code !== 'Escape') return;
+        instance.close(() => document.removeEventListener('keydown', pressEscapeHandller));
+    };
+
+    instance.show(() => document.addEventListener('keydown', pressEscapeHandller));
+};
+
+gallery.insertAdjacentHTML('beforeend', markup);
+gallery.addEventListener('click', showImageHandller);
---
+++ js/02-lightbox.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+)
+    .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const settingsSimpleLightbox = event => {
+    event.preventDefault();
+    gallery.removeEventListener('click', settingsSimpleLightbox);
+
+    new SimpleLightbox('.gallery__link',
+        {
+            captionsData: 'alt',
+            captionDelay: 250
+        }
+    );
+};
+
+gallery.addEventListener('click', settingsSimpleLightbox);
