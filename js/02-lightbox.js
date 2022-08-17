2efafbd67cfa843319bb90cc0c26fca9c8d15b68
varunyk1805 <varunyk1805@gmail.com>
2022-08-17 13:34:18 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,25 +2,23 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
-const markup = galleryItems.map(({ preview, original, description }) => 
-    `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__link" href="${original}">
             <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-    </li>`
-)
-    .join('');
+    </a>`,
+  )
+  .join('');
 
 const settingsSimpleLightbox = event => {
-    event.preventDefault();
-    gallery.removeEventListener('click', settingsSimpleLightbox);
+  event.preventDefault();
+  gallery.removeEventListener('click', settingsSimpleLightbox);
 
-    new SimpleLightbox('.gallery__link',
-        {
-            captionsData: 'alt',
-            captionDelay: 250
-        }
-    );
+  new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 };
 
 gallery.insertAdjacentHTML('beforeend', markup);
